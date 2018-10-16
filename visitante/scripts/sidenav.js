2bac4664efee8a1da07c0656ec04d6cbb48acd2b
Alejandro <24446995+0xalen@users.noreply.github.com>
2018-10-16 09:46:21 -0300

    Side Menu

---
+++ visitante/scripts/sidenav.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.getElementById("sideMenu").style.width = "85%";
+     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeMenu() {
+    document.getElementById("sideMenu").style.width = "0";
+    document.body.style.backgroundColor = "white";
+} 
