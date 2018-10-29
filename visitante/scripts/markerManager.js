2443707ee7d507920811d56813b0c7c6075791ff
Alejandro <24446995+0xalen@users.noreply.github.com>
2018-10-29 14:35:36 -0300

    new file names

---
+++ visitante/scripts/markerManager.js
@@ -0,0 +1,32 @@
+function createNewMarker(id, elementName, patternURL) {
+            var obj = {};
+    obj.id = id;
+    obj.elementName = elementName;
+    obj.patternURL = patternURL;
+
+    return obj;
+}
+
+function createMarkersManager() {
+    var obj = {};
+    obj.markerList = [];
+}
+
+function addMarker(list, marker) {
+    list.push(marker);
+    return list;
+}
+
+function removeMarker(list, ) {
+    list
+}
+
+function searchElementIndex(list, element) {
+    if (list.indexOf !== undefined) {
+        list.indexOf(element);
+    } else {
+
+    }
+}
+
+
