dde3d9ae014a3313c4e249832d13796bfa55cd51
Alejandro <24446995+0xalen@users.noreply.github.com>
2018-10-29 12:40:48 -0300

    visitorManager added

---
+++ visitante/scripts/visitorManager.js
@@ -0,0 +1,32 @@
+function createVisitorManager() {
+    var obj = {};
+    obj.markersLoaded = false;
+
+    return obj;
+}
+
+function searchForMarkers(list) {
+    var i;
+    for (i = 0; i < list.length; i++) {
+       replaceMarkerPattern(list[i]);
+       replaceName(list[i]);
+    }
+}
+
+function replaceMarkerPattern(marker) {
+    pattSrc = marker.patternURL;
+    document.getElementById('amarkerID').url=pattSrc;
+}
+
+function replaceName(marker) {
+    eName = marker.elementName;
+    document.getElementById('').elementName=eName;
+}   
+
+function checkMarkers(visitorManager) {
+    if (visitorManager.markersLoaded = false) {
+
+    } else {
+
+    }
+}
