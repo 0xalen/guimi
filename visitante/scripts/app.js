8c4138ae93955320895f0b4cc6bb6b34d47cc38c
Alejandro <24446995+0xalen@users.noreply.github.com>
2018-10-30 10:29:25 -0300

    events moved to main app

--- visitante/scripts/app.js
+++ visitante/scripts/app.js
@@ -1,5 +1,5 @@
 // Copyright 2016 Google Inc.
-//			 2018 Franco Alejandro Trinidad
+//            2018 Franco Alejandro Trinidad
 //				  María Luz Almada
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,13 +15,65 @@
 // limitations under the License.
 
 (function() {
-  'use strict';
+    'use strict';
  
- var app = {
-    isLoading: true,
-    spinner: document.querySelector('.loader'),
-    container: document.querySelector('.main'),
-    addDialog: document.querySelector('.dialog-container'),
-  };  
+    var app = {
+        isLoading: true,
+        spinner: document.querySelector('.loader'),
+        container: document.querySelector('.main'),
+        addDialog: document.querySelector('.dialog-container'),
+    };
   
+    /**********************************************************
+    *
+    *   Events
+    *
+    ***********************************************************/
+
+    AFRAME.registerComponent('registerevents', {
+        init: function() {
+            var marker = this.el;
+            marker.setAttribute('emitevents', 'true');
+            marker.addEventListener('markerFound', function() {
+                var markerID = marker.id;
+                console.log('markerFound', markerId);
+                // Cuando se encuentra un marcador
+            });
+            marker.addEventListener('markerLost', function() {
+                var markerId = marker.id;
+                console.log('markerLost', markerId);
+                // Cuando se pierde un marcador
+            });
+        }
+    });
+
+    /**********************************************************
+    *
+    *   Methods
+    *
+    ***********************************************************/
+    // Check state
+    var checkState = function (v) {
+        if (!v.checkLoaded()) {
+            v.loadMarkers();
+        } else if (v.getContentType === 0 || v.getContentType === 1 ||
+                    v.getContentType === 2 || v.getContentType === 3) {
+            v.loadContent();
+        } else if (v.checkChosen()) {
+            v.loadOptions();
+        } else {
+            v.searchForMarkers();
+        }
+    }
+
+    /**********************************************************
+    *
+    *   Main
+    *
+    ***********************************************************/
+    var visitor = new VisitorManager();
+    checkState();
+
+
+
 })();
--- visitante/scripts/visitorManager.js
+++ visitante/scripts/visitorManager.js
@@ -1,30 +1,105 @@
-function createVisitorManager() {
-    this.markersLoaded = false;
+function VisitorManager() {
+    var markersLoaded = false;
+    var markersListM;
+    var markersDetectedM;
+    // var markersDetected = 0; reemplazado por markersDetectedM.getLength
+    var elementChosen = false;
+    var resourceM;
+    var contentType = -1;
+    var viewingContent = false;
 
-}
 
-function searchForMarkers(list) {
-    var i;
-    for (i = 0; i < list.length; i++) {
-       replaceMarkerPattern(list[i]);
-       replaceName(list[i]);
+    // Check state
+    this.checkLoaded = function() {
+        return markersLoaded;
     }
-}
+    this.checkChosen = function() {
+        return elementChosen;
+    }
 
-function replaceMarkerPattern(marker) {
-    pattSrc = marker.patternURL;
-    document.getElementById('amarkerID').url=pattSrc;
-}
+    // MARKERS
+    // Load markers
+    this.loadMarkers = function() {
+        markerListM = createMarkerManager();
+        markerListM.setMarkers();
+        markersLoaded = true;
+    }
+    var createMarkerManager = function() {
+        return new MarkersManager();
+    }
 
-function replaceName(marker) {
-    eName = marker.elementName;
-    document.getElementById('').elementName=eName;
-}   
+    // Search for markers in the scene
+    this.searchForMarkers = function(list) {
+        var i;
+        for (i = 0; i < list.length; i++) {
+            replaceMarkerPattern(list[i]);
+            replaceName(list[i]);
+            checkDetected(list[i]);
+        }
+    }
+    var replaceMarkerPattern = function(marker) {
+        pattSrc = marker.patternURL;
+        document.getElementById('amarkerID').url=pattSrc;
+    }
+    var replaceName = function(marker) {
+        eName = marker.elementName;
+        document.getElementById('').elementName=eName;
+    }
 
-function checkMarkers(visitorManager) {
-    if (visitorManager.markersLoaded = false) {
+    var checkDetected = function(marker) {
+        if (markersDetected == undefined) {
+            markerMD = createMarkerManager();
+        }
 
-    } else {
+        /* //Check if marker is recognized
+        if (){
+            marker.setDetected(true);
+            markerMD.addMarker(marker);
+        }*/
+    }
 
+    // CONTENT
+    var cType = {
+        IMAGE: 0,
+        TEXT: 1,
+        VIDEO: 2,
+        AUDIO: 3,
+    }
+
+    this.setChosen = function() {
+        if (markersDetectedM.getLength() === 1) {
+            elementChosen = true;
+            return;
+        }
+            elementChosen = false;
+    }
+
+    this.loadOptions = function() {
+        var chosenID = markersDetectedM.getMarkers()[0].getID();
+        resourceM = createResourceManager(chosenID);
+    }
+
+    this.selectContentType = function(c) {
+        setContentType(c);
+        loadContent();
+    }
+
+    var setContentType = function(c) {
+        contentType = c;
+    }
+    var loadContent = function() {
+        resourceM.setContent(contentType);
+    }
+
+    this.getContentType = function() {
+        return contentType;
+    }
+
+    this.displayContent = function() {
+        var cList = resourceM.getContent();
+    }
+
+    var createResourceManager = function(elementID) {
+        return new ResourcesManager(elementID);
     }
 }
