acf6bfb7f5f3f7cf590a193568bbb5bad4a70cc5
Alejandro <24446995+0xalen@users.noreply.github.com>
2018-11-06 16:04:54 -0300

    optionsScreen

---
+++ visitante/scripts/optionsScreen.js
@@ -0,0 +1,24 @@
+
+
+/* Option menu events */
+    document.getElementById("typeImage").addEventListener("click", function() {
+        console.log('Image content selected');
+        openContentGallery(0);
+    });
+    document.getElementById("typeText").addEventListener("click", function() {
+        console.log('Text content selected');
+        openContentGallery(1);
+    });
+    document.getElementById("typeVideo").addEventListener("click", function() {
+        console.log('Video content selected');
+        openContentGallery(2);
+    });
+    document.getElementById("typeAudio").addEventListener("click", function() {
+        console.log('Audio content selected');
+        openContentGallery(3);
+    });
+
+    document.getElementById("goBack").addEventListener("click", function() {
+        console.log('Close menu');
+        closeContentGallery();
+    });
