b004ab273de4bc6a93a11b957e3f6c4f32bc408a
Alejandro <24446995+0xalen@users.noreply.github.com>
2018-11-06 14:09:04 -0300

    Copyright notice changed

--- visitante/scripts/MarkersManager.js
+++ visitante/scripts/MarkersManager.js
@@ -1,7 +1,9 @@
-/** Copyright 2018 Franco Alejandro Trinidad
- *                 María Luz Almada
+/**
+ * GuIMI: Guía Interactiva del Museo de Informática (UNPA-UARG)
+ * Copyright (C) 2018 Franco Alejandro Trinidad
+ *                    María Luz Almada
  *
- * GuIMI is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
@@ -12,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with GuIMI. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  **/
 function MarkersManager() {
     var markerList = new MarkerList();
