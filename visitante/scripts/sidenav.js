7628c644d85de3778241eb0f31ea4c2f959f98a1
Alejandro <24446995+0xalen@users.noreply.github.com>
2018-11-12 13:44:38 -0300

    side menu functions

--- visitante/scripts/sidenav.js
+++ visitante/scripts/sidenav.js
@@ -1,9 +1,11 @@
 function openMenu() {
-    document.getElementById("sideMenu").style.width = "250px";
-     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("side-menu").style.width = "250px";
+    document.getElementById('main').style.marginLeft = '250px';
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeMenu() {
-    document.getElementById("sideMenu").style.width = "0";
+    document.getElementById("side-menu").style.width = "0";
+    document.getElementById('main').style.marginLeft = '0';
     document.body.style.backgroundColor = "white";
 } 
