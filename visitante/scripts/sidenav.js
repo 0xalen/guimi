e6caf39e07dbba4419dc6b2978a1bbfe4831900f
Alejandro <24446995+0xalen@users.noreply.github.com>
2018-11-23 16:14:00 -0300

    hide main when showing sidenav

--- visitante/scripts/sidenav.js
+++ visitante/scripts/sidenav.js
@@ -1,11 +1,13 @@
 function openMenu() {
     document.getElementById("side-menu").style.width = "250px";
-    document.getElementById('main').style.marginLeft = '250px';
+    //document.getElementById('main').style.marginLeft = '250px';
+    document.getElementById('main').style.display = none;
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeMenu() {
     document.getElementById("side-menu").style.width = "0";
-    document.getElementById('main').style.marginLeft = '0';
+    //document.getElementById('main').style.marginLeft = '0';
+    document.getElementById('main').style.display = block;
     document.body.style.backgroundColor = "white";
 } 
