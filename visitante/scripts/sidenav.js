36706d904eab9b69862740bad029aa30e7caf8f2
Alejandro <24446995+0xalen@users.noreply.github.com>
2018-11-23 16:39:53 -0300

    none with quotes

--- visitante/scripts/sidenav.js
+++ visitante/scripts/sidenav.js
@@ -1,7 +1,7 @@
 function openMenu() {
     document.getElementById("side-menu").style.width = "250px";
     //document.getElementById('main').style.marginLeft = '250px';
-    document.getElementById('main').style.display = none;
+    document.getElementById('main').style.display = "none";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
