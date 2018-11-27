78120f14d2ac1792b74ea083d73549009e21cc06
Alejandro <24446995+0xalen@users.noreply.github.com>
2018-11-27 02:19:53 -0300

    service worker path to net

--- visitante/scripts/app.js
+++ visitante/scripts/app.js
@@ -197,7 +197,8 @@
 	console.log("Checking for Service Worker");
 	if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./../service-worker.js')
+             //.register('./../service-worker.js')
+             .register('https://raw.githubusercontent.com/0xalen/guimi/master/service-worker.js')
              .then(function() { console.log('Service Worker registered'); });
   }
 
