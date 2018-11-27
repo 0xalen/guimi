73cc800b447809b512a1f1c4406b84f2736b6baa
Alejandro <24446995+0xalen@users.noreply.github.com>
2018-11-27 12:00:51 -0300

    out of place code removed

--- visitante/scripts/app.js
+++ visitante/scripts/app.js
@@ -90,9 +90,6 @@
                 console.log('Marker counter: ' + app.markerCounter);    //DEBUG
         });
     }
-    document.querySelector('p').addEventListener('click', function (evt) {
-        console.log('This 2D element was clicked!');
-    });
 
     /**********************************************************
     *
